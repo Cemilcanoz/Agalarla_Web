5d1e238d7fff484155f5f09b23d205970ea3fe66
cemobaba <cemilcanoz15@gmail.com>
2025-12-20 23:01:10 +0300

    2
    deneme2

--- Data/Interfaces/ICategoryRepository.cs
+++ Data/Interfaces/ICategoryRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace Agalarla_Web.Data.Interfaces
+﻿using Agalarla_Web.Models;
+
+namespace Agalarla_Web.Data.Interfaces
 {
     public interface ICategoryRepository
     {
+        // Sistemdeki tüm kategorileri (Alkollü, Alkolsüz vb.) getirmek için kullanılır
+        IEnumerable<Category> Categories { get; }
     }
 }
--- Data/Interfaces/IDrinkRepository.cs
+++ Data/Interfaces/IDrinkRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace Agalarla_Web.Data.Interfaces
+﻿using Agalarla_Web.Models;
+
+namespace Agalarla_Web.Data.Interfaces
 {
     public interface IDrinkRepository
     {
+        // Tüm içecekleri liste olarak döndürür
+        IEnumerable<Drink> Drinks { get; }
+
+        // Sadece IsPreferredDrink değeri true olan (öne çıkan) içecekleri getirir
+        IEnumerable<Drink> PreferredDrinks { get; }
+
+        // ID numarasına göre tek bir içeceğin bilgisini getirir
+        Drink GetDrinkById(int drinkId);
     }
 }
